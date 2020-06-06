0de971c0dca3ba24b2fb8d695cfede5b20f7c000
dgotlieb <Dgotlieb8@gmail.com>
2020-06-06 20:10:56 +0300

    Create Solution.java

---
+++ Class1/Solution.java
@@ -0,0 +1,56 @@
+public class First {
+    public static void main(String[] args) {
+        // 1
+        int x = 7;
+        double d = 44.3;
+        String s = "abc";
+
+        //2
+        System.out.println(x + d);
+        System.out.println(x + s);
+
+        //3
+        // a = 9, b = 8, c = 15
+
+        //4
+        /*
+        * int
+        * double
+        * char
+        * float
+        * long
+        * byte
+        * short
+        * boolean
+        * */
+
+        //5
+        // A local variable in Java is a variable that’s declared within the body of a method.
+        // Those variables can only be used within that method.
+        // Other methods in the class can't use this variable.
+
+        //6
+        // A global variable in Java is a variable that’s declared within a class.
+        // Those variables can be accessed from all methods within that class.
+
+        //7
+        // 2, 2.0
+
+        //8
+        // 1 String only
+
+        //9
+        // 7
+
+        //10
+        //8
+
+        //11
+        // Option 1: String hello = "hello ";
+        // Option 2: String world = " world";
+        // Option 3: System.out.println(hello + " " + world);
+
+        //12
+        // super is a Java reserved word and can't be used by us.
+    }
+}
