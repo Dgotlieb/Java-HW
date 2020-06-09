433ac7d41986dedf82440dce24937da8264888cc
dgotlieb <Dgotlieb8@gmail.com>
2020-06-09 21:51:58 +0300

    Create Challenge.java

---
+++ Class4/Challenge/Challenge.java
@@ -0,0 +1,14 @@
+package challenges.fourth;
+
+public class Challenge {
+    public static void main(String[] args) throws MyException {
+        //13
+        // In Java, const is a reserved keyword and not being used.
+        // In C language, const is often used to declare a constant.
+        // However, in Java, final is used instead.
+
+        //14
+        throw new MyException("My exception");
+    }
+}
+
